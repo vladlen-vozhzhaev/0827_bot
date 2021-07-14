6e7edf77a547cc2a06bcaed51cfff0a6cf99eeae
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2021-07-14 11:09:20 +0300

    Update googleBot.js
    Добавлена возможность работы с несколькими сайтами
    Исправлены некоторые ошибки

--- googleBot.js
+++ googleBot.js
@@ -6,24 +6,39 @@
 // @author       You
 // @match        https://www.google.com/*
 // @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
+// @match        https://crushdrummers.ru/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
 let googleInput = document.getElementsByName("q")[0];
 let btnK = document.getElementsByName("btnK")[1];
+let sites = {
+    "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai": ["Как звучит гобой","Флейта","Кларнет","Фагот","Валторна","Саксофон"],
+    "crushdrummers.ru": ["Барабанное шоу", "Шоу барабанщиков Crush", "Заказать барабанное шоу"]
+}
+let site = Object.keys(sites)[getIntRandom(0, Object.keys(sites).length)]; // Возвращаем случайный сайт
+let words = sites[site]; // Получаем набор коючевых слов одного из сайтов
+//let words = ["Как звучит гобой","Флейта","Кларнет","Фагот","Валторна","Саксофон"]; // Набор коючевых слов
+let word = words[getIntRandom(0, words.length)]; // Получаем случайное слово из массива words
 
 if(btnK != undefined){ // Проверяем, что мы на главной странице
-    googleInput.value = "гобой"; // Пишем фразу в поисковую строку
-    setTimeout(function(){
-        btnK.click();// Клик по кнопке поиска
-    }, 1000);
+    let i=0;
+    let timerId = setInterval(function(){
+        googleInput.value = googleInput.value + word[i++]; // Пишем по буквам фразу в поисковую строку
+        document.cookie = "site="+site; // Записали выбранный сайт в cookie браузера
+        if(i==word.length){
+            clearInterval(timerId);
+            btnK.click();// Клик по кнопке поиска
+        }
+    }, 500);
 }else if(location.hostname === "www.google.com"){ // Если страница с поисковой выдачей
     let links = document.links; // Собираем коллекцию ссылок
     let goNext = true;
+    let site = getCookie("site"); // Достаём ранее выбранный сайт из куки
     for(let i=0; i<links.length; i++){ // Перебираем ссылки
         let link = links[i];
-        if(link.href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") != -1){ // Ищем ссылку с нужным сайтом
+        if(link.href.indexOf(site) != -1){ // Ищем ссылку с нужным сайтом
             setTimeout(function(){
                 link.click(); // Кликаем по ссылке с нужным сайтом
             }, 3000);
@@ -32,25 +47,37 @@ if(btnK != undefined){ // Проверяем, что мы на главной с
         }
     }
     if(goNext){ // Проверяем, можно ли идти далее по страницам поисковика
-        let pnnext = document.getElementById("pnnext"); // Находим кнопку "Следующая"
-        setTimeout(function(){
-            pnnext.click(); // Кликаем по кнопке следующая
-        }, 3000);
+        let currentPage = document.querySelector('.YyVfkd').innerText;
+        if(currentPage<10){
+            let pnnext = document.getElementById("pnnext"); // Находим кнопку "Следующая"
+            setTimeout(function(){
+                pnnext.click(); // Кликаем по кнопке следующая
+            }, 3000);
+        }else{
+            location.href = "https://www.google.com/";
+        }
     }
 }else{ // Любой другой сайт
-    let links = document.links; // Коллекция ссылок
-    let randomIndex = getIntRandom(0, links.length);
-    let link = links[randomIndex];
-    if(link.href.indexOf(location.hostname) != -1){ // Если переход внутри сайта
-        setTimeout(function(){
+    setInterval(function(){
+        if(getIntRandom(0,100)<30) location.href = "https://www.google.com/"; // С некоторой вероятностью мы уйдём на сайт google
+        let links = document.links; // Коллекция ссылок
+        let randomIndex = getIntRandom(0, links.length);
+        let link = links[randomIndex];
+        if(link.href.indexOf(location.hostname) != -1){ // Если переход внутри сайта
             links[randomIndex].click();
-        }, 2000);
-    }else{ // Если переход на другой сайт, то мы ссылаем браузер на главную страницу нашего сайта
-        location.href = "https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/";
-    }
+        }else{ // Если переход на другой сайт, то мы ссылаем браузер на главную страницу нашего сайта
+            location.href = location.origin;
+        }
+    },2000);
 }
 
 function getIntRandom(min, max){
     return Math.floor(Math.random()*(max-min)+min);
 }
 
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
